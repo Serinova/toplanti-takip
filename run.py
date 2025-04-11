54a0a0e5a16af9f1c2a7bcdca789a843b0f03cc2
Serinova <serinova@outlook.com.tr>
2025-04-11 22:34:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-from app import create_app, db
-from flask_migrate import upgrade
+from flask import Flask
+app = Flask(__name__)
 
-app = create_app()
+@app.route("/")
+def home():
+    return "Hello, Vercel!"
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
